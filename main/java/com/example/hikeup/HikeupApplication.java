472d526d7d0faf19d71c11ee7011882a04dcfcbf
sasi-bit <55438592+sasi-bit@users.noreply.github.com>
2022-01-02 07:49:52 +0530

    Add files via upload

---
+++ main/java/com/example/hikeup/HikeupApplication.java
@@ -0,0 +1,13 @@
+package com.example.hikeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeupApplication.class, args);
+	}
+
+}
---
+++ test/java/com/example/hikeup/HikeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hikeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
